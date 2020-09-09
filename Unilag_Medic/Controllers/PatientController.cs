70e80e54cc4e6d0fe1cd090e1840061a638e7ee0
Larhwally <lawalwhylawal@gmail.com>
2020-09-09 01:53:58 +0100

    remove createdBy from patient

--- Unilag_Medic/Controllers/PatientController.cs
+++ Unilag_Medic/Controllers/PatientController.cs
@@ -65,7 +65,7 @@ namespace Unilag_Medic.Controllers
 
                 Dictionary<string, object> genericPatient = new Dictionary<string, object>();
 
-                string[] patientRecord = { "surname", "otherNames", "phoneNumber", "altPhoneNum", "email", "ethnicGroup", "gender", "hospitalNumber", "nhisNumber", "hmoId", "dateOfBirth", "maritalStatus", "address", "stateId", "nationalityId", "patientType", "nokName", "nokAddress", "nokPhoneNum", "nokRelationship", "faculty", "department", "status", "createdBy" };
+                string[] patientRecord = { "surname", "otherNames", "phoneNumber", "altPhoneNum", "email", "ethnicGroup", "gender", "hospitalNumber", "nhisNumber", "hmoId", "dateOfBirth", "maritalStatus", "address", "stateId", "nationalityId", "patientType", "nokName", "nokAddress", "nokPhoneNum", "nokRelationship", "faculty", "department", "status" };
 
                 genericPatient = Utility.Pick(param, patientRecord);
 
