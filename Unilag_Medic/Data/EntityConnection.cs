04bb8f967630a590c4c2f8ea039dca802feb705d
Larhwally <lawalwhylawal@gmail.com>
2020-09-12 20:28:18 +0100

    fixed update password

--- Unilag_Medic/Data/EntityConnection.cs
+++ Unilag_Medic/Data/EntityConnection.cs
@@ -424,9 +424,9 @@ namespace Unilag_Medic.Data
         public bool UpdateUser(int id, UnilagMedLogin unilag)
         {
             this.connection.Open();
-            string query = "UPDATE tbl_medicalstaff SET password = @password, updatedBy = @updatedBy, createDate = @createDate WHERE medstaffId = @medstaffId";
+            string query = "UPDATE tbl_medicalstaff SET password = @password, updatedBy = @updatedBy, createDate = @createDate WHERE itbId = @itbId";
             MySqlCommand command = new MySqlCommand(query, this.connection);
-            MySqlParameter parameter = new MySqlParameter("@medstaffId", MySqlDbType.Int32);
+            MySqlParameter parameter = new MySqlParameter("@itbId", MySqlDbType.Int32);
             byte[] salt = { 2, 3, 1, 2, 3, 6, 7, 4, 2, 3, 1, 7, 8, 9, 6 };
             //using (var rng = RandomNumberGenerator.Create())
             //{
