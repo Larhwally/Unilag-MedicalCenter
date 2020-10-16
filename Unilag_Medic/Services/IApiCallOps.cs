619d88f6f8716a12cc39ae383234ca2e19a850b8
Larhwally <lawalwhylawal@gmail.com>
2020-10-16 15:07:35 +0100

    update IApicallOps interface

--- Unilag_Medic/Services/IApiCallOps.cs
+++ Unilag_Medic/Services/IApiCallOps.cs
@@ -1,3 +1,9 @@
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unilag_Medic.Models;
+
 namespace Unilag_Medic.Services
 {
     public interface IApiCallOps
