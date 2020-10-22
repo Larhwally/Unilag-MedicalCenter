e80b3534307242eaa5af1101a99579c25dfde5b7
Larhwally <lawalwhylawal@gmail.com>
2020-10-22 18:48:49 +0100

    Added endpoint for FileUpload

--- Unilag_Medic/Controllers/FileUpload.cs
+++ Unilag_Medic/Controllers/FileUpload.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
